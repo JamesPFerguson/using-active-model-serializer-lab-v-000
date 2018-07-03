8f7822c8ccc09adf4418f68f8f382918d20d5afa
James Ferguson <ncjamesferguson@gmail.com>
2018-07-03 23:04:01 +0000

    automatically backed up by learn

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :created_at
+  attributes :id
   has_many :products, through: :ordered_products
   class ProductSerializer < ActiveModel::Serializer
     attributes :name
